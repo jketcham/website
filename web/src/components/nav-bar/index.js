a3017868575a43fdcaf3c422f15de257afa19e5d
Jack Ketcham <kcajketcham@gmail.com>
2018-04-30 02:09:04 -0700

    Add style for nav-bar.

--- web/src/components/nav-bar/index.js
+++ web/src/components/nav-bar/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import styles from './nav-bar.css';
-
+import { navBar, navBarHeader } from './nav-bar.css';
+import { listInline } from '../shared.css';
 
 const Navbar = () => (
-  <div className="nav-bar">
-    <h3>jack ketcham</h3>
-    <ul className="list-inline">
+  <div className={navBar}>
+    <h3 className={navBarHeader}>jack ketcham</h3>
+    <ul className={listInline}>
       <li><a href="#about">about</a></li>
       <li><a href="#photos">photos</a></li>
       <li><a href="#blog">blog</a></li>
     </ul>
   </div>
 );
-
 
 export default Navbar;
