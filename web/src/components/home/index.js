22672cc3871fc3ad6dffa874c9d61a18d0c0be81
Jack Ketcham <kcajketcham@gmail.com>
2019-01-23 12:36:53 -0700

    Add rel="me" to profile links

--- web/src/components/home/index.js
+++ web/src/components/home/index.js
@@ -6,20 +6,22 @@ const HomePage = () => (
   <div className={page}>
     <h1>Hi</h1>
     <div>
-      <p>I&apos;m Jack.</p>
+      <p>
+        I&apos;m Jack, a software engineer living in Tempe, Arizona.
+      </p>
       <ul>
         <li>
-          <a href="https://github.com/jketcham" rel="noopener" className={link}>
+          <a href="https://github.com/jketcham" rel="me noopener" className={link}>
             GitHub
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/_jket" rel="noopener" className={link}>
+          <a href="https://twitter.com/_jket" rel="me noopener" className={link}>
             Twitter
           </a>
         </li>
         <li>
-          <a href="https://flickr.com/photos/jackketcham" rel="noopener" className={link}>
+          <a href="https://flickr.com/photos/jackketcham" rel="me noopener" className={link}>
             Flickr
           </a>
         </li>
