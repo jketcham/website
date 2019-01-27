e67bb0cc276ee74ee0d17e0e126db87f78196d0d
Jack Ketcham <kcajketcham@gmail.com>
2019-01-27 13:59:28 -0700

    Update setting NODE_ENV

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -45,9 +45,7 @@ const config = {
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
     new webpack.LoaderOptionsPlugin({
       debug: isDev,
