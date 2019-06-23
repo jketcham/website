cf74fbe480b2f0777017d9cfb0eec664d48bd090
Jack Ketcham <jack@jackketcham.com>
2019-06-22 21:41:17 -0700

    Load dot environment file in config

--- website/config.py
+++ website/config.py
@@ -1,5 +1,11 @@
-import os
+from os import environ
+from os.path import join, dirname
 import importlib
 
-env = os.environ.get('ENV', 'default')
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+env = environ.get('ENV', 'default')
 config = importlib.import_module('config.' + env)
