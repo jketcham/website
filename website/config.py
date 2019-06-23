9487f82ef4e01fba366703283423b7c9968e877c
Jack Ketcham <jack@jackketcham.com>
2019-06-23 00:21:27 -0700

    Update path to dot env file

--- website/config.py
+++ website/config.py
@@ -1,11 +1,9 @@
 from os import environ
-from os.path import join, dirname
 import importlib
 
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv('.env')
 
 env = environ.get('ENV', 'default')
 config = importlib.import_module('config.' + env)
